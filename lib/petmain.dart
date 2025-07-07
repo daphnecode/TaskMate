2b2071b2989380536f41e8bd0679a577d8378605
daphnecode <daphnecode@gmail.com>
2025-07-07 16:43:57 +0900

    add tutorial dialog

--- lib/petmain.dart
+++ lib/petmain.dart
@@ -41,7 +41,45 @@ class Mainarea extends StatelessWidget {
                       child: IconButton(
                         icon: Icon(Icons.question_mark,),
                         onPressed: () {
-                          // Handle back button press
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.all(16.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Image.asset(
+                                        "assets/images/petTuto1.png",
+                                        height: 600.0,
+                                        width: 250.0,
+                                      ),
+                                      Text(
+                                        "펫 키우기 메인화면 입니다.",
+                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(height: 10.0),
+                                      Text(
+                                        "여러 기능을 사용할 수 있습니다.",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                      SizedBox(height: 20.0),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text("닫기"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
                         },
                         )
                       ),
