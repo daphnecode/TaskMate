ce2c338f7bea987886da4fd9d2b5f353b3db1f78
daphnecode <daphnecode@gmail.com>
2025-09-04 21:20:49 +0900

    add levelExp & modify updatestatus entire process

--- functions/src/pet/updateStatus.ts
+++ functions/src/pet/updateStatus.ts
@@ -1,6 +1,7 @@
 // src/index.ts
 import { onSchedule } from "firebase-functions/v2/scheduler";
 import { db } from "../firebase";
+import { getLevelExp } from "./levelExp";
 
 // 매일 새벽 0시 (UTC 기준) 실행
 export const updateStatus = onSchedule({schedule: "0 */1 * * *", timeZone: "Asia/Seoul"}, 
@@ -33,6 +34,7 @@ export const updateStatus = onSchedule({schedule: "0 */1 * * *", timeZone: "Asia
         const petHunger = petData["hunger"] ?? 0;
         const petHappy = petData["happy"] ?? 0;
         const petExp = petData["currentExp"] ?? 0;
+        const petLevel = petData["level"] ?? 0;
 
         // 3. 상태 감소 로직 (예: hunger -24, happy -30)
         /* 
@@ -40,21 +42,48 @@ export const updateStatus = onSchedule({schedule: "0 */1 * * *", timeZone: "Asia
         행복도 0 이하일 때, 경험치 감소 추가 필요.
         */
         
-        const newHunger = Math.max(0, petHunger - 2);
-        const newHappy = Math.max(0, petHappy - 2);
-        let updates: any = { hunger: newHunger, happy: newHappy , currentExp: newExp};
+        
+        let updates: any = {};
 
-	if (newHunger === 0) {
-	  const lostHappy = (petHunger === 0) ? 2 : (newHunger - petHunger);
-	  const newExp = Math.max(0, petExp - lostHappy);
-	  updates.currentExp = newExp;
-	}
+        // Hunger 감소
+        var newHunger = petHunger - 2;
+        var newHappy = petHappy - 2;
+        if (newHunger < 0) {
+          const deficit = Math.abs(newHunger);
+          newHappy -= deficit;
+          updates.hunger = 0;
+        } else {
+          updates.hunger = newHunger;
+        }
+
+        // Happy 감소
+        var newExp = petExp;
+        if (newHappy < 0) {
+          const deficit = Math.abs(newHappy);
+          newExp -= deficit;
+          updates.happy = 0;
+        } else {
+          updates.happy = newHappy;
+        }
+
+        // Exp 감소
+        var newLevel = petLevel;
+        if (newLevel <= 1 && newExp < 0) {
+          updates.newExp = 0;
+        } else if (newExp < 0 && newLevel > 1) {
+          newLevel -= 1; // 레벨 감소
+          const levelExp = getLevelExp(newLevel);
+          newExp += levelExp// 음수 보정
+          updates.currentExp = newExp;
+          updates.level = newLevel;
+        } else {
+          updates.currentExp = newExp;
+        }
         
         
         await petRef.set(updates, { merge: true });
         
 
-        console.log(`유저 ${userId}의 펫 ${nowPetId} 의 currentExp : ${newExp}`);
         console.log(`유저 ${userId}의 펫 ${nowPetId} 상태 갱신됨:`, updateStatus);        
       }
 
